ea6aa929a24c57f1e25c178762bc33d4bfe471df
loxygenK <loxygen.k@gmail.com>
2020-07-09 23:32:37 +0900

    †編集†　KeyCapクラスでのdraw時に色を指定できるようにした

--- src/main/kotlin/game/drawable/KeyCap.kt
+++ src/main/kotlin/game/drawable/KeyCap.kt
@@ -1,5 +1,6 @@
 package game.drawable
 
+import java.awt.Color
 import java.awt.Graphics2D
 import java.awt.Rectangle
 
@@ -8,8 +9,19 @@ class KeyCap(
    private val rectangle: Rectangle
 ) {
 
-   fun draw(graphics: Graphics2D) {
+   fun draw(
+      graphics: Graphics2D,
+      background: Color = Color(0, 0, 0, 0),
+      borderColor: Color = Color.GRAY,
+      textColor: Color = Color.GRAY
+   ) {
+      graphics.color = background
+      graphics.fillRect(0, 0, rectangle.width, rectangle.height)
+
+      graphics.color = borderColor
       graphics.drawRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height)
+
+      graphics.color = textColor
       graphics.drawString(
          caption.toString(),
          rectangle.x + rectangle.width / 2 - 7, rectangle.y + rectangle.height / 2 + 9
