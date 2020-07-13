9d797aa5047a26c48eb2bb39092900d74891b214
loxygenK <loxygen.k@gmail.com>
2020-07-13 20:37:19 +0900

    †追加† ファイルから読むようにした

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 import game.Game
+import game.sheet.SheetMusicParser
+import java.io.File
 
 fun main() {
-   Game().start()
+
+   val musicSheet = SheetMusicParser.parse(
+      File("test/kkiminochikara.tlm")
+   )
+   Game(musicSheet).start()
+
 }
